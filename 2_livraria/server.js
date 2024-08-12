d0f7da9373da419042a23c206c614b0489900cb1
mraphaely <raphaelymaryana@gmail.com>
2024-08-12 15:05:07 -0300

    add rotas clientes livraria

--- 2_livraria/server.js
+++ 2_livraria/server.js
@@ -332,7 +332,7 @@ app.put("/funcionarios/:id", (request, response) => {
 app.delete("/funcionarios/:id", (request, response) => {
     const id = request.params.id
     const sql = /*sql*/`DELETE FROM funcionarios WHERE funcionario_id = "${id}"`
-    connection.query(sql, (err, data) => {
+    connection.query(sql, (err, info) => {
         if (err) {
             console.error(err);
             response.status(500).json({ err: "Erro ao deletar funcionário." })
@@ -357,4 +357,137 @@ app.get("/funcionarios/:id", (request, response) => {
         const funcionario = data[0]
         response.status(200).json(funcionario);
     })
+});
+// (MVC) model view controller
+/**************** Rotas Clientes *********************/
+/* tabela (cliente_id, nome, email, created_at, updated_at)
+   1° Listar todos os clientes
+   2° Cadastrar um cliente (email é único)
+   3° Listar um cliente
+   4° Atualizar um cliente (não pode ter o email de outro func.)
+   5° Deletar um cliente
+ */
+app.get("/clientes", (request, response) => {
+    const selectSQL = /*sql*/`SELECT * FROM clientes`
+    connection.query(selectSQL, (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao selecionar clientes" });
+            return
+        }
+        const clientes = data
+        response.status(200).json(clientes);
+    })
+});
+app.post("/clientes", (request, response) => {
+    const { nome, email } = request.body
+    if (!nome) {
+        return response.status(400).json({ message: "O nome é obrigatório" })
+    }
+    if (!email) {
+        return response.status(400).json({ message: "O email é obrigatório" })
+    }
+    if (!email.includes("@")) {
+        return response.status(400).json({ message: "O email deve conter '@'" })
+    }
+    const checkEmailSQL = /*sql*/`SELECT * FROM clientes WHERE email = "${email}"`
+    connection.query(checkEmailSQL, (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao verificar email" });
+            return
+        }
+        if (data.length > 0) {
+            return response.status(400).json({ message: "Email já está cadastrado." })
+        }
+
+        const id = uuidv4();
+        const insertSQL = /*sql*/`INSERT INTO clientes (cliente_id, nome, email)
+        VALUES ("${id}", "${nome}", "${email}")`
+        connection.query(insertSQL, (err, data) => {
+            if (err) {
+                console.error(err);
+                response.status(500).json({ err: "Erro ao cadastrar cliente" });
+                return
+            }
+            response.status(201).json({ message: "Cliente cadastrado com sucesso" });
+        });
+    });
+});
+app.put("/clientes/:id", (request, response) => {
+    const { id } = request.params
+    const { nome, email } = request.body
+    if (!nome) {
+        return response.status(400).json({ message: "O nome é obrigatório" })
+    }
+    if (!email) {
+        return response.status(400).json({ message: "O email é obrigatório" })
+    }
+    if (!email.includes("@")) {
+        return response.status(400).json({ message: "O email deve conter '@'" })
+    }
+    const CheckClienteSQL = /*sql*/`SELECT * FROM clientes WHERE cliente_id = "${id}"`
+    connection.query(CheckClienteSQL, (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao verificar cliente" });
+            return
+        }
+        if (data.length === 0) {
+            return response.status(404).json({ message: "Cliente não encontrado" })
+        }
+    });
+    const checkEmailSQL = /*sql*/`SELECT * FROM clientes WHERE email = "${email}" AND cliente_id != "${id}"`
+    connection.query(checkEmailSQL, (err, data) => {
+        if (err) {
+            console.error(err)
+            response.status(500).json({ err: "Erro ao procurar cliente" })
+            return
+        }
+        if (data.length > 0) {
+            response.status(409).json({ err: "Email já existe" });
+        }
+    });
+    const updateSQL = /*sql*/`UPDATE clientes SET nome = "${nome}", email = "${email}"
+    WHERE cliente_id = "${id}"
+    `
+    connection.query(updateSQL, (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao atualizar cliente" });
+            return
+        }
+        response.status(200).json({ message: "Cliente atualizado com sucesso" });
+    });
+});
+app.delete("/clientes/:id", (request, response) => {
+    const { id } = request.params
+    const deleteClienteSQL = /*sql*/`DELETE FROM clientes WHERE cliente_id = "${id}"
+    `
+    connection.query(deleteClienteSQL, (err, info) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao deletar cliente" });
+            return
+        }
+        if (info.affectedRows) {
+            response.status(404).json({ message: "Cliente não encontrado." })
+        }
+        response.status(200).json({ message: "Cliente deletado com sucesso." })
+    })
+});
+app.get("/clientes/:id", (request, response) => {
+    const { id } = request.params
+    const clienteSQL = /*sql*/`SELECT * FROM clientes WHERE cliente_id = "${id}"`
+    connection.query(clienteSQL, (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).json({ err: "Erro ao selecionar cliente" })
+            return
+        }
+        const cliente = data[0]
+        response.status(200).json(cliente);
+
+    })
+
 });
