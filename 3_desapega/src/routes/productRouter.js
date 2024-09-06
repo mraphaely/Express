6775d503becd1fae6ee37de12ac167a37e56ecb4
mraphaely <raphaelymaryana@gmail.com>
2024-09-06 15:22:44 -0300

    xxx

--- 3_desapega/src/routes/productRouter.js
+++ 3_desapega/src/routes/productRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+
 import { cadastroProduto, listarProdutos } from "../controllers/productController.js";
+//middlewares
+import verifyToken from "../helpers/verify-token.js"
 
 const router = Router();
 
-router.post("/cadastroproduto", cadastroProduto);
+router.post("/cadastroproduto", verifyToken,cadastroProduto);
 router.get("/listarproduto", listarProdutos);
 
 export default router;
